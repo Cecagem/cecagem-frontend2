dd09e7320a46b039c959ef417e9624f5a4592111
dahe-dev <anadventure19@gmail.com>
2025-08-30 18:21:47 -0500

    fix: suspense login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import { LoginForm } from "@/features/auth";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
       <div className="w-full max-w-5xl">
-        <LoginForm />
+        <Suspense fallback={<div>Cargando...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
