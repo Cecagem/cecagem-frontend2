8dba9cf9bbd9fb3ec97531d5795b3fd6ab36d977
dahe-dev <anadventure19@gmail.com>
2025-09-29 02:26:27 -0500

    fix: nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/cecagem/:path*",
-        // destination: "https://back.ecomassystems.com/api/v1/:path*",
-        // destination: "http://localhost:3001/api/v1/:path*",
-        destination: "https://back-system.cecagem.com/api/v1/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/cecagem/:path*",
+  //       // destination: "https://back.ecomassystems.com/api/v1/:path*",
+  //       // destination: "http://localhost:3001/api/v1/:path*",
+  //       destination: "https://back-system.cecagem.com/api/v1/:path*",
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
